4313e0e89dce5d7e89d7f6c30ec5512c22bd542d
Silvio <silvio.dias@etec.sp.gov.br>
2021-09-25 03:31:29 -0300

    creating dashboard page

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,25 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
+  return (
+    <Flex align="center">
+      {showProfileData && (
+          <Box mr="4" textAlign="right">
+            <Text>Silvio Dias</Text>
+            <Text
+              color="gray.300"
+              fontSize="small"
+            >
+              silviodiasjr@outlook.com
+            </Text>
+          </Box>
+        )}
+
+      <Avatar size="md" name="Silvio Dias" src="https://github.com/silviodiasjr.png" />
+    </Flex>
+  )
+}
